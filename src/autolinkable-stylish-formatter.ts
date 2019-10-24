496d27ca472780e5b2dac77f7d3f61d124f08362
Alexander T <alexander.tarasyuk@outlook.com>
2019-10-24 11:42:35 +0300

    add prettier

--- src/autolinkable-stylish-formatter.ts
+++ src/autolinkable-stylish-formatter.ts
@@ -31,35 +31,35 @@ const ERROR = 'ERROR';
 
 const isError = (severity: number) => severity === 2;
 
-const pad = (str: string, minLen: number, maxLen: number) => (
-  maxLen <= minLen
-    ? str
-    : `${ str }${ ' '.repeat(maxLen - minLen) }`
-);
+const pad = (str: string, minLen: number, maxLen: number) =>
+  maxLen <= minLen ? str : `${str}${' '.repeat(maxLen - minLen)}`;
 
 const buildPath = (filePath: string, line: number, column: number) => {
-  const path = !filePath.includes('/') && !filePath.includes('\\')
-    ?`.${ sep }${ filePath }`
-    : filePath;
+  const path =
+    !filePath.includes('/') && !filePath.includes('\\')
+      ? `.${sep}${filePath}`
+      : filePath;
 
-  return [
-    path,
-    line ? `:${ line }`: '',
-    column ? `:${ column }`: '',
-  ].join('');
-}
-
-const getErrorType = (severity: number, isRaw = true) => {
-  const type = isRaw
-    ? isError(severity) ? ERROR : WARNING
-    : isError(severity) ? chalk.red(ERROR) : chalk.blue(WARNING);
-
-  return `${ type }: `;
+  return [path, line ? `:${line}` : '', column ? `:${column}` : ''].join('');
 };
 
-const buildMessage = ({ errorType, message, rule, path }: Message, maxPathLen: number, maxRuleLen: number) => (
-  `${ errorType }${ pad(path.filePath, path.len, maxPathLen) } ${ pad(rule.name, rule.len, maxRuleLen) } ${ message }`
-);
+const getErrorType = (severity: number, isRaw = true) => {
+  if (isRaw) {
+    return `${isError(severity) ? ERROR : WARNING}: `;
+  }
+  return `${isError(severity) ? chalk.red(ERROR) : chalk.blue(WARNING)}: `;
+};
+
+const buildMessage = (
+  { errorType, message, rule, path }: Message,
+  maxPathLen: number,
+  maxRuleLen: number,
+) =>
+  `${errorType}${pad(path.filePath, path.len, maxPathLen)} ${pad(
+    rule.name,
+    rule.len,
+    maxRuleLen,
+  )} ${message}`;
 
 const buildOutput = (result: CLIEngine.LintResult): string => {
   const initialData: InitialData = {
@@ -68,37 +68,43 @@ const buildOutput = (result: CLIEngine.LintResult): string => {
     messages: [],
   };
 
-  const {
-    maxRuleLen,
-    maxPathLen,
-    messages,
-  } = result.messages.reduce(({ maxRuleLen, maxPathLen, messages }, { severity, message, ruleId, column, line }) => {
-    const errorType = getErrorType(severity);
-    const path = buildPath(result.filePath, line, column);
+  const { maxRuleLen, maxPathLen, messages } = result.messages.reduce(
+    (
+      { maxRuleLen, maxPathLen, messages },
+      { severity, message, ruleId, column, line },
+    ) => {
+      const errorType = getErrorType(severity);
+      const path = buildPath(result.filePath, line, column);
 
-    const ruleName = ruleId ? ruleId : '';
-    const ruleLen = ruleName.length;
-    const pathLen = errorType.length + path.length;
+      const ruleName = ruleId ? ruleId : '';
+      const ruleLen = ruleName.length;
+      const pathLen = errorType.length + path.length;
 
-    return {
-      maxRuleLen: Math.max(maxRuleLen, ruleLen),
-      maxPathLen: Math.max(maxPathLen, pathLen),
-      messages: [...messages, {
-        errorType: getErrorType(severity, false),
-        message: chalk.yellow(message),
-        rule: { name: chalk.grey(ruleName), len: ruleLen },
-        path: { filePath: path, len: pathLen },
-      }],
-    };
-  }, initialData);
+      return {
+        maxRuleLen: Math.max(maxRuleLen, ruleLen),
+        maxPathLen: Math.max(maxPathLen, pathLen),
+        messages: [
+          ...messages,
+          {
+            errorType: getErrorType(severity, false),
+            message: chalk.yellow(message),
+            rule: { name: chalk.grey(ruleName), len: ruleLen },
+            path: { filePath: path, len: pathLen },
+          },
+        ],
+      };
+    },
+    initialData,
+  );
 
   return [
-    chalk.white(result.filePath), ...messages.map(message => buildMessage(message, maxPathLen, maxRuleLen)),
+    chalk.white(result.filePath),
+    ...messages.map(message => buildMessage(message, maxPathLen, maxRuleLen)),
   ].join(SEPARATOR);
-}
+};
 
-export = (results: CLIEngine.LintResult[]) => (
+export = (results: CLIEngine.LintResult[]) =>
   results
     .filter(result => result.errorCount || result.warningCount)
-    .map(buildOutput).join(SEPARATOR)
-);
+    .map(buildOutput)
+    .join(SEPARATOR);
--- tests/autolinkable-stylish-formatter.test.ts
+++ tests/autolinkable-stylish-formatter.test.ts
@@ -5,9 +5,11 @@ import parsingErrorsReport from './__fixtures__/parsing-errors-report.json';
 describe('autolinkable-stylish-formatter', () => {
   it('matches its snapshot', () => {
     expect(autolinkableStylishFormatter(report)).toMatchSnapshot();
-  })
+  });
 
   it('matches its snapshot with parsing errors', () => {
-    expect(autolinkableStylishFormatter(parsingErrorsReport as any)).toMatchSnapshot();
-  })
-})
+    expect(
+      autolinkableStylishFormatter(parsingErrorsReport as any),
+    ).toMatchSnapshot();
+  });
+});
