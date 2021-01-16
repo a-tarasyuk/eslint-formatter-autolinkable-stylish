0e8a2c2638dd4cccd569386fe3b116b08fc36e9c
Oleksandr T <oleksandr.tarasiuk@outlook.com>
2021-01-16 18:10:46 +0200

    migrate to GitHub actions

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,9 @@ module.exports = {
   testEnvironment: 'node',
   transform: { '^.+\\.ts$': 'ts-jest' },
   testRegex: './tests/.+\\.test\\.ts$',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  }
 }
